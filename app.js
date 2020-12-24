1870ec547ed769c5d006f77f2ba4517844a09a1f
Vickram Rameshbabu <vickramwww@gmail.com>
2020-12-24 16:53:37 -0500

    hello world node app

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+var request = (request, response) => {
+    response.writeHead("200", {
+        'Content-Type':'text/plain'
+    });
+    response.write("<h1>This is my test node App</h1>");
+    response.end();
+}
+
+http.createServer(request).listen(8080);
+console.log("Listening on 8080..");
